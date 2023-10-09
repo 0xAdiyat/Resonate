0258f0bca3e2b4c991dd23b87640e28c91046527
0xadiyat <0xAdiyat@gmail.com>
2023-10-09 09:07:48 +0600

    Refactor PasswordStrengthIndicator Widget for Improved Color Handling with Improved logic
    Updated the PasswordStrengthIndicator widget to better handle the color representation of password strength based on the number of valid checks. The widget now correctly displays amberRed, yellow, or green colors for varying degrees of password strength, and gray when no checks are true. Added comments to improve code readability.
    
    refactors #165

--- lib/views/screens/signup_screen.dart
+++ lib/views/screens/signup_screen.dart
@@ -157,7 +157,8 @@ class _SignupScreenState extends State<SignupScreen> {
                         child: Padding(
                           padding: EdgeInsets.only(top: UiSizes.height_20),
                           child: SizedBox(
-                              height: UiSizes.height_45,
+                              height: UiSizes.height_40,
+                              width: Get.width,
                               child: PasswordStrengthIndicator(
                                 isPasswordSixCharacters:
                                     passwordStrengthCheckerController
@@ -176,12 +177,15 @@ class _SignupScreenState extends State<SignupScreen> {
                                     "Include at least 1 uppercase letter",
                                 hasLowerCaseTitle:
                                     "Include at least 1 lowercase letter",
+                                validatedChecks:
+                                    passwordStrengthCheckerController
+                                        .validatedChecks.value,
                               )),
                         ),
                       ),
                     ),
                   ),
-                  SizedBox(height: UiSizes.height_24_6),
+                  SizedBox(height: UiSizes.height_20),
                   Obx(
                     () => ElevatedButton(
                       onPressed: emailVerifyController.signupisallowed.value
